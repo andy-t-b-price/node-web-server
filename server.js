567e55d2ff221a8943576d48eccbb89bb1ef94c8
TOTEMIC\andy.price <andy.price@totemic.co.uk>
2017-06-14 09:59:33 +0100

    adding handlebars

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
-app.get('/', (req, res) => {
-    // res.send('<h1>Hello express!</h1>');
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
 
-    res.send({
-        name: 'Andy',
-        hobbies: [
-            'Rugby',
-            'Gym'
-        ]
-    })
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to the home page'
+    });   
 });
 
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+   res.render('about.hbs', {
+       pageTitle: 'About Page'
+       
+   });
 });
 
 app.get('/bad', (req, res) => {
@@ -26,4 +38,6 @@ app.get('/bad', (req, res) => {
 });
 
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+});
